cc07b584cc4deee58e5ef70c23602899e2282cc7
Ban Markovic <banmarkovic>
2026-09-22 13:17:06 +0200

    Enable R8 optimization for release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,9 @@ android {
     buildTypes {
         release {
             optimization {
-                enable = false
+                enable = true
             }
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
