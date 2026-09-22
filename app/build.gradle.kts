610eceffc436013561200b82d7d292ba2295b7bc
Ban Markovic <banmarkovic>
2026-09-22 14:37:39 +0200

    Remove unused dependency and opt out of auto backup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,6 @@ dependencies {
     implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.runtime)
